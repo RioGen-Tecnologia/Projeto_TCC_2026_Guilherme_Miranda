1b0f2848de38e2414cce37578f63a044fb180858
MirandaGuilherme19 <guilherme.miranda.moret@outlook.com>
2026-05-23 19:24:31 -0300

    Create batch_correction_analysis.r

---
+++ scripts/batch_correction_analysis.r
@@ -0,0 +1,9 @@
+# 
+# Análise de correção de efeito de batch
+# Guilherme Moret Miranda - Riogen
+# 23/05/2026
+# Esse script carrega dados de expressão de cada dataset obtido na análise principal
+# para integrar-los e corrigir seus efeitos de batch pelo pacote sva (comBat)
+# para visualizar a capacidade que as amostras tem de separar e os grupos tumor
+# e não-tumor.
+
